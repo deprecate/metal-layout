79ec9547d4d2bf7cadbac8ef8212bd2469906d00
zxdgoal <john.zhao@liferay.com>
2016-06-24 14:05:15 +0800

    init commit

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var metal = require('gulp-metal');
+
+metal.registerTasks({
+	bundleCssFileName: 'myComponent.css',
+	bundleFileName: 'myComponent.js',
+	moduleName: 'metal-myComponent'
+});
